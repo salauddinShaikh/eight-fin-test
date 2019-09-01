4de396e88e5996200c880001d689fa05bf4b51a1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-01 08:50:06 +0000

    Deploy salauddinShaikh/eight-fin-test to github.com/salauddinShaikh/eight-fin-test.git:gh-pages

---
+++ precache-manifest.cafc0322e340d6b2281d129fdaef9689.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb6be7d6c0e122fcf070fd20395a444",
+    "url": "/eight-fin-test/index.html"
+  },
+  {
+    "revision": "b96b00fcdefa2fc806fd",
+    "url": "/eight-fin-test/static/css/main.71d19787.chunk.css"
+  },
+  {
+    "revision": "cc636d5542f454b92cd2",
+    "url": "/eight-fin-test/static/js/2.b5291daf.chunk.js"
+  },
+  {
+    "revision": "b96b00fcdefa2fc806fd",
+    "url": "/eight-fin-test/static/js/main.c9ef7ffb.chunk.js"
+  },
+  {
+    "revision": "9886429006e2fef71663",
+    "url": "/eight-fin-test/static/js/runtime~main.abc77f33.js"
+  }
+]);
